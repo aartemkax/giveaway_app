d8f623a1f72f86d363ab5e552974d61bd235cce1
Aartemkax <aartemkax@gmail.com>
2025-05-06 00:44:37 +0300

    Fix deduplication syntax and integrate API_BASE_URL in Participant.fromJson

--- lib/services/participants_service.dart
+++ lib/services/participants_service.dart
@@ -1,10 +1,9 @@
-// lib/services/api_service.dart
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/participant.dart';
 import '../utils/constants.dart';
 
+/// Надсилає POST на /api/fetch_participants і повертає список учасників.
 Future<List<Participant>> fetchParticipants(String postUrl) async {
   final uri = Uri.parse('$apiBaseUrl/api/fetch_participants');
   final resp = await http.post(
@@ -12,13 +11,10 @@ Future<List<Participant>> fetchParticipants(String postUrl) async {
     headers: {'Content-Type': 'application/json'},
     body: jsonEncode({'post_url': postUrl}),
   );
-
   if (resp.statusCode != 200) {
-    throw Exception('Failed to fetch: ${resp.statusCode}');
+    throw Exception('HTTP ${resp.statusCode}: ${resp.body}');
   }
-  final body = jsonDecode(resp.body) as Map<String, dynamic>;
-  final list = body['participants'] as List<dynamic>;
-  return list
-      .map((j) => Participant.fromJson(j as Map<String, dynamic>))
-      .toList();
+  final json = jsonDecode(resp.body) as Map<String, dynamic>;
+  final list = json['participants'] as List<dynamic>;
+  return list.map((e) => Participant.fromJson(e)).toList();
 }
