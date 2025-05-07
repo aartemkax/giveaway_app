7300f968f9f45cad78d41b363547a0c26466dcb8
Aartemkax <aartemkax@gmail.com>
2025-05-07 10:54:52 +0300

    Fix Dockerfile: use relative paths in api context

--- lib/services/participants_service.dart
+++ lib/services/participants_service.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import '../models/participant.dart';
 import '../utils/constants.dart';
 
-/// Надсилає POST на /api/fetch_participants і повертає список учасників.
 Future<List<Participant>> fetchParticipants(String postUrl) async {
   final uri = Uri.parse('$apiBaseUrl/api/fetch_participants');
   final resp = await http.post(
@@ -11,10 +10,21 @@ Future<List<Participant>> fetchParticipants(String postUrl) async {
     headers: {'Content-Type': 'application/json'},
     body: jsonEncode({'post_url': postUrl}),
   );
-  if (resp.statusCode != 200) {
-    throw Exception('HTTP ${resp.statusCode}: ${resp.body}');
+  if (resp.statusCode == 200) {
+    final data = jsonDecode(resp.body) as Map<String, dynamic>;
+    return (data['participants'] as List)
+        .map((j) => Participant.fromJson(j))
+        .toList();
   }
-  final json = jsonDecode(resp.body) as Map<String, dynamic>;
-  final list = json['participants'] as List<dynamic>;
-  return list.map((e) => Participant.fromJson(e)).toList();
+  final err = jsonDecode(resp.body)['error'] as String;
+  switch (resp.statusCode) {
+    case 400:
+      throw Exception('Неправильний запит: $err');
+    case 403:
+      throw Exception('Доступ заборонено: $err');
+    case 429:
+      throw Exception('Забагато запитів: $err');
+    default:
+      throw Exception('Серверна помилка ${resp.statusCode}: $err');
+  }
 }
